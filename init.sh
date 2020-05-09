92573f84e9d30f8e3b4f689794cc1db34ffaf9fb
CastHash532 <51857536+CastHash532@users.noreply.github.com>
2020-05-09 16:55:20 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 sudo wget https://downloads.partkeepr.org/partkeepr-1.4.0.zip && \
 sudo unzip -qq partkeepr-1.4.0.zip -d ./ && \
 sudo rm -f partkeepr-1.4.0.zip && \
-sudo mv ./partkeepr-1.4.0 ./PartKeepr
+sudo mv ./partkeepr-1.4.0 ./PartKeepr && \
+docker-compose up --build
