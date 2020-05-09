164abfe2efaccbe4dcff62b74f68768bf9bf2517
Casthash532 <gm_sahraoui@esi.dz>
2020-05-09 16:50:56 +0200

    add init.sh to import app

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo wget https://downloads.partkeepr.org/partkeepr-1.4.0.zip && \
+sudo unzip -qq partkeepr-1.4.0.zip -d ./ && \
+sudo rm -f partkeepr-1.4.0.zip && \
+sudo mv /partkeepr-1.4.0 /PartKeepr
